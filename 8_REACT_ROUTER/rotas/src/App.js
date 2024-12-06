1deca25475702b36aa29a904a1f9671a1ecee459
Luis Otávio Miranda Amaro <luis.amaro@agv.intranet>
2024-12-06 11:15:23 -0300

    ROTAS / 13 - Redirecionamento de URL

--- 8_REACT_ROUTER/rotas/src/App.js
+++ 8_REACT_ROUTER/rotas/src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 
 // 1 - config react router 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 //components
 import Navbar from './components/Navbar';
@@ -31,6 +31,8 @@ function App() {
             <Route path='/products/:id' element ={<Product/>}/>
             {/**9 - search */}
             <Route path='/search' element={<Search />} />
+            {/**10 - = redirect*/}
+            <Route path='/company' element={<Navigate to='/about'/>} />
             {/*7 - no match route*/}
             <Route path='*' element={<NotFound/>} />
         </Routes>
