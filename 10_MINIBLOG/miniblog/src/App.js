da4a0b861ea615d7135a6d8dc773b02dbb213f53
Luis Otávio Miranda Amaro <luis.amaro@agv.intranet>
2024-12-11 17:35:24 -0300

    PROJETO MINIBLOG / 5 - Configurando o React Router

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+//pages
+
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+      <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+      </div>
+      </BrowserRouter>
+      
     </div>
   );
 }
