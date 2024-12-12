acb3982f11e25eb96919c9cda98fb589ad09d0c9
Luis Otávio Miranda Amaro <luis.amaro@agv.intranet>
2024-12-12 17:35:42 -0300

    PROJETO MINIBLOG / 13 - Registrando usuário no sistema

--- 10_MINIBLOG/miniblog/src/pages/Register/Register.js
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.js
@@ -1,7 +1,10 @@
+import { db } from '../../firebase/config'
+
 import React from 'react'
 import styles from './Register.module.css'
 
 import {useState, useEffect} from 'react'
+import { useAuthentication } from '../../hooks/useAuthentication'
 
 const Register = () => {
   const [displayName, setDisplayName] = useState("")
@@ -9,8 +12,10 @@ const Register = () => {
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState("")
+
+  const {createUser, error: authError, loading} = useAuthentication()
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault()
 
     setError("")
@@ -25,8 +30,16 @@ const Register = () => {
         setError("As senhas precisam ser iguais")
         return 
     }
-        console.log(user)
+        const res = await createUser(user)
+
+        console.log(res)
   }
+
+  useEffect(() => {
+    if(authError) {
+        setError(authError)
+    }
+  },[authError])
 
     return (
     <div className={styles.register}>
@@ -77,7 +90,12 @@ const Register = () => {
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 />
             </label>
-            <button className="btn">Cadastrar</button>
+            {!loading && <button className="btn">Cadastrar</button>}
+            {loading && (
+                <button className='btn' disabled>
+                    Aguarde...
+                </button>
+            )}
             {error && <p className="error">{error}</p>}
         </form>
     </div>
