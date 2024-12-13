cbaa02dc68adf55249c9d4d0f7ef8c3b8af0d3f0
Luis Otávio Miranda Amaro <luis.amaro@agv.intranet>
2024-12-13 08:55:55 -0300

    PROJETO MINIBLOG / 15 - Passando usuário para o contexto

--- 10_MINIBLOG/miniblog/src/context/AuthContext.js
+++ 10_MINIBLOG/miniblog/src/context/AuthContext.js
@@ -1,11 +1,12 @@
-import {useContext, createContext} from 'react'
+import {useContext, createContext} from 'react' // Importa os hooks `useContext` e `createContext` da biblioteca React
 
-const AuthContext = createContext()
+const AuthContext = createContext() // Cria um contexto chamado `AuthContext`, que será usado para gerenciar e compartilhar dados globalmente
 
-export function AuthProvider({children, value}) {
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+export function AuthProvider({children, value}) { // Define um componente chamado `AuthProvider` que aceita `children` e `value` como props
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider> // Retorna um provedor de contexto, compartilhando o `value` com os componentes filhos
 }
 
-export function useAuthValue() {
-    return useContext(AuthContext)
+export function useAuthValue() { // Define um hook personalizado chamado `useAuthValue` para acessar o contexto
+    return useContext(AuthContext) // Usa o hook `useContext` para obter o valor atual de `AuthContext`
 }
+ 
