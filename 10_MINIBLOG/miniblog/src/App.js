1b87e389b432342476d6c2e6c23951d136fd9247
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2024-12-20 11:11:54 -0300

    PROJETO MINIBLOG / 31 - Criando página de post individual

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,65 +1,74 @@
-import './App.css';
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 //hooks
-import {useState, useEffect} from 'react'
-import { useAuthentication } from './hooks/useAuthentication';
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 //context
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from "./context/AuthContext";
 
 //pages
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Register from './pages/Register/Register';
-import Login from './pages/Login/Login';
-import Dashboard from './pages/Dashboard/Dashboard';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Search from './pages/Search/Search';
-
-
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
 
-  const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication ()
-
-  const loadingUser = user === undefined
+  const loadingUser = user === undefined;
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
-    })
-  }, [auth])
+      setUser(user);
+    });
+  }, [auth]);
 
   if (loadingUser) {
-    return <p>Carregando...</p>
+    return <p>Carregando...</p>;
   }
 
   return (
     <div className="App">
-      <AuthProvider value={{user}}>
-      <BrowserRouter>
-      <NavBar/>
-      <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/search' element={<Search/>} />
-        <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>} />
-        <Route path='/register' element={!user ? <Register/> : <Navigate to="/"/>} />
-        <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="login"/>}/>
-        <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to="/login"/>}/>
-      </Routes>
-      </div>
-      <Footer/>
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
-      
     </div>
   );
 }
