54e005039eef2d7c2f7a6f7e4d43aeb7a0d09918
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2024-12-23 17:53:29 -0300

    PROJETO MINIBLOG / 35 - Exibindo posts do usuário na dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,37 +1,37 @@
-import styles from './Dashboard.module.css'
+import styles from "./Dashboard.module.css";
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-//hooks 
-import { useAuthValue } from '../../context/AuthContext' 
-import { useFetchDocuments } from '../../hooks/useFetchDocuments'
-
+//hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.id
+  const { user } = useAuthValue();
+  const uid = user.id;
 
-
-  //posts do usuario 
-  const posts = []
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
   return (
     <div>
-        <h2>Dashboard</h2>
-        <p>Gerencie os seus posts</p>
-        {posts && posts.length === 0 ? (
-          <div clasName={styles.noposts} >
-            <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className="btn" >Criar primeiro post</Link>
-          </div>
-        ) : (
-          <div>
-            <p>Tem posts</p>
-          </div>
-        )}
-    </div>
-  )
-}
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div clasName={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts</p>
+        </div>
+      )}
 
-export default Dashboard
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
+    </div>
+  );
+};
+
+export default Dashboard;
