83738d0201efdf9ff064d3842fa5d12988d89b29
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2024-12-23 17:42:05 -0300

    PROJETO MINIBLOG / 34 - Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,35 @@
 import styles from './Dashboard.module.css'
 
-import {useState} from "react"
-import {useNavigate} from "react-router-dom"
-import { useAuthentication } from '../../hooks/useAuthentication'
+import {Link} from "react-router-dom"
+
+
+//hooks 
+import { useAuthValue } from '../../context/AuthContext' 
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 
 const Dashboard = () => {
-  
+  const {user} = useAuthValue()
+  const uid = user.id
 
+
+  //posts do usuario 
+  const posts = []
 
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div clasName={styles.noposts} >
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn" >Criar primeiro post</Link>
+          </div>
+        ) : (
+          <div>
+            <p>Tem posts</p>
+          </div>
+        )}
     </div>
   )
 }
