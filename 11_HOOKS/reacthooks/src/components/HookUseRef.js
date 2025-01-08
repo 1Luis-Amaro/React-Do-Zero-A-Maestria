58fc928de6276d3502152969870f337fd8847817
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-01-08 09:46:57 -0300

    HOOKS / 12 - useRef e Dom

--- 11_HOOKS/reacthooks/src/components/HookUseRef.js
+++ 11_HOOKS/reacthooks/src/components/HookUseRef.js
@@ -7,9 +7,21 @@ const HookUseRef = () => {
   const [counterB, setCounterB] = useState(0);
 
   useEffect(() => {
-    numberRef.current = numberRef.current + 1
-    
-  })
+    numberRef.current = numberRef.current + 1;
+  });
+
+  //2 - useRef e DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputRef)
+
+    setText("")
+
+    inputRef.current.focus()
+  }
 
   return (
     <div>
@@ -19,6 +31,18 @@ const HookUseRef = () => {
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 2: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      {/**2 - useRef e DOM */}
+      <form onSubmit={handleSubmit} >
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
       <hr />
     </div>
   );
