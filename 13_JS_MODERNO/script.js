5917e25b1907d10dc981c7ea2cfa3bbb2a4cf08f
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-02-27 17:07:44 -0300

    Javascript Moderno / 04 - Arrow functions

--- 13_JS_MODERNO/script.js
+++ 13_JS_MODERNO/script.js
@@ -29,3 +29,48 @@ for(let i=0; i < 5; i++){
 
 console.log(i)
 
+//2 arrow function
+const sum = function sum(a, b) {
+    return a + b
+}
+
+const arrowSum = (a, b) => a + b
+
+console.log(sum(5,5))
+console.log(arrowSum(5,5))
+
+const greeting = (name) => {
+    if(name) {
+        return 'Olá' + name + "!"
+    }else {
+        return "olá"
+    }
+}
+
+console.log(greeting(" Matheus"))
+console.log(greeting())
+
+const testArrow = () => console.log("testou!")
+
+testArrow()
+
+const user = {
+    name: 'Theo',
+    sayUserName() {
+        var self = this
+        setTimeout(function() {
+            console.log(self)
+            console.log('Username' + self.name)
+        },500)
+    },
+    sayUserNameArrow() {
+        setInterval(() => {
+            console.log(this)
+            console.log('Username ' + this.name)
+        },700)
+    }
+}
+
+
+
+
