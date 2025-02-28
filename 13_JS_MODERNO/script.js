72f964e173fd69247b87b6723a75cb1e65bbbcd6
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-02-28 10:21:20 -0300

    Javascript Moderno / 08 - Destructuring

--- 13_JS_MODERNO/script.js
+++ 13_JS_MODERNO/script.js
@@ -120,4 +120,26 @@ products.map((product) => {
 const username = "luis"
 const age = 20
 
-console.log(`O nome do usuário é ${username} e a idade é ${age} anos`)
+//console.log(`O nome do usuário é ${username} e a idade é ${age} anos`)
+
+
+//6 destruturing
+const fruits = ["Maçã", "Laranja", "Mamão"]
+
+const [f1, f2, f3] = fruits
+
+console.log(f1)
+console.log(f2)
+console.log(f3)
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Cinza"
+
+}
+
+const {name: productName, price, category: productCategory, color} = productDetails
+
+console.log(`O nome do produto é ${productName}, custa R$${price} pertence a categoria ${productCategory} e é da cor ${color}`)
