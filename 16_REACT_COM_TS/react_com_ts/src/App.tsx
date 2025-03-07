bcd98e28ba18094cd7d97ca0a466de9a540b913e
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-07 15:40:09 -0300

    React Com TypeScript / 05 - Váriaveis no React

--- 16_REACT_COM_TS/react_com_ts/src/App.tsx
+++ 16_REACT_COM_TS/react_com_ts/src/App.tsx
@@ -1,9 +1,21 @@
-
-
 function App() {
+  //1 - variaveis
+  const name: string = "Luis";
+  const age: number = 30;
+  const isWorking: boolean = false;
+
   return (
     <div className="App">
-        <h1>TypeScript com React</h1>
+      <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking ? (
+        <div>
+          <p>Está trabalhando</p>
+        </div>
+      ) : (
+        <p>Não está trabalhando</p>
+      )}
     </div>
   );
 }
