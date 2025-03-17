52d9d2c44aea264f86b3ff4c9a5ddfe6264312b3
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-17 17:51:30 -0300

    Projeto de React com TypeScript / 05 - Criando formulário

--- 17_ Projeto_React_com_TS/projeto/src/App.tsx
+++ 17_ Projeto_React_com_TS/projeto/src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <main className={styles.main}></main>
-       <h1>Conteudo...</h1>
-       <Footer/>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa"/>
+        </div>
+          <h2>Suas tarefas</h2>
+          <TaskList/>
+        <div></div>
+      </main>
+      <Footer />
     </div>
   );
 }
