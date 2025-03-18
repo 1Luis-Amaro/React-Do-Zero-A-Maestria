302aeb3e4d1365b17263c11fa963b958422e9159
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-18 17:20:50 -0300

    Projeto de React com TypeScript / 12 - Removendo tarefa

--- 17_ Projeto_React_com_TS/projeto/src/components/TaskList.tsx
+++ 17_ Projeto_React_com_TS/projeto/src/components/TaskList.tsx
@@ -1,38 +1,42 @@
-import React from 'react'
+import React from "react";
 
 //CSS
-import styles from './TaskList.module.css'
+import styles from "./TaskList.module.css";
 
 //interfaces
-import { ITask } from '../interfaces/Task'
-
-
+import { ITask } from "../interfaces/Task";
 
 type Props = {
-    taskList: ITask[]
-}
+  taskList: ITask[];
+  handleDelete(id: number): void;
+};
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = ({ taskList, handleDelete }: Props) => {
   return (
     <>
-        {taskList.length > 0 ? (
-            taskList.map((task) => (
-                <div key={task.id} className={styles.task} >
-                    <div className={styles.details} >
-                        <h4>{task.title}</h4>
-                        <p>Dificuldade: {task.difficulty}</p>
-                    </div>
-                    <div className={styles.actions} >
-                        <i className="bi bi-pencil"></i>
-                        <i className="bi bi-trash"></i>
-                    </div>
-                </div>
-            ))
-        ): (
-            <p>Não há tarefas cadastradas!</p>
-        )}
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil"></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
