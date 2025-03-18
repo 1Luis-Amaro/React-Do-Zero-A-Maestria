de825483ca77240e90f29de126b2791e6d07e39f
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-18 10:21:54 -0300

    Projeto de React com TypeScript / 07 - Iniciando ações do formulário

--- 17_ Projeto_React_com_TS/projeto/src/components/TaskForm.tsx
+++ 17_ Projeto_React_com_TS/projeto/src/components/TaskForm.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useState, ChangeEvent, FormEvent, useEffect} from "react";
 
 //CSS
 import styles from './TaskForm.module.css'
+
+//Interface
+import {ITask} from '../interfaces/Task'
 
 interface Props {
   btnText: string;
 }
 
 const TaskForm = ({ btnText }: Props) => {
+
+    const [id, setId] = useState<number>(0)
+    const [title, setTitle] = useState <string>("")
+    const [dificulty, setDificulty] = useState<number>()
+
+const addTaskHandler = () => {
+
+}
+
+const hadleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "title") {
+        setTitle(e.target.value)
+    }else {
+        setDificulty(parseInt (e.target.value))
+    }
+    console.log(title)
+    console.log(dificulty)
+}
+
   return (
-    <form className={styles.form} >
+    <form onSubmit={ addTaskHandler } className={styles.form} >
       <div className={styles.input_container}  >
         <label htmlFor="title">Título</label>
-        <input type="text" name="title" placeholder="Título da tarefa" />
+        <input type="text" name="title" placeholder="Título da tarefa" onChange={hadleChange} />
       </div>
       <div className={styles.input_container}  >
         <label htmlFor="dificulty">Dificuldade:</label>
@@ -20,6 +42,7 @@ const TaskForm = ({ btnText }: Props) => {
           type="text"
           name="dificulty"
           placeholder="Dificuldade da tarefa"
+          onChange={hadleChange}
         />
       </div>
       <input type="submit" value={btnText} />
