aa723ebc26b2dbc3e346048ec70b6a9ea9f17976
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-19 08:38:34 -0300

    Projeto de React com TypeScript / 16 - Preenchendo modal com dados

--- 17_ Projeto_React_com_TS/projeto/src/App.tsx
+++ 17_ Projeto_React_com_TS/projeto/src/App.tsx
@@ -13,6 +13,7 @@ import Modal from "./components/Modal";
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
 
   const deleteTask = (id: number) =>  {
     setTaskList(
@@ -31,13 +32,14 @@ function App() {
     }
   }
 
-  const editTask = ():void => {
+  const editTask = (task: ITask):void => {
     hideOrShowModal(true)
+    setTaskToUpdate(task)
   }
 
   return (
     <div>
-      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />} />
+      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} task={taskToUpdate} />} />
       <Header />
       <main className={styles.main}>
         <div>
